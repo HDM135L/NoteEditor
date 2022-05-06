2f2544bfc77235e0347f2041bc622c105d94dd49
Tom <tomwxh@163.com>
2022-05-07 00:18:08 +0800

    add(tk): interface of node editor

--- TKinterface.py
+++ TKinterface.py
@@ -2,27 +2,66 @@ from tkinter import *
 from tkinter import ttk
 
 
-class CLS_setter(object):
+class CLS_NoteEditor(object):
     def __init__(self):
         version = "v0.1"
         self.root = Tk()
         self.root.title("MUNECK Node Editor"+version)
-        self.mainframe = ttk.Frame(root, padding="3 3 12 12")
-        self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(0, weight=1)
-def calculate(*args):
-    try:
-        value = float(feet.get())
-        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
-    except ValueError:
-        pass
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+        self.noteFrame = ttk.Frame(self.root, padding="3 3 12 12")
+        self.noteFrame.grid(column=1, row=1,  columnspan=8, rowspan=9, sticky=(N, W, E, S))
+        #note type /radioButton
+        self.noteType = StringVar()
+        self.noteType.set('flick')
+        type_flick = ttk.Radiobutton(self.noteFrame, text='Flick', variable=self.noteType, value='flick')
+        type_flick.grid(row=2, column=1, rowspan=1, columnspan=2)
+        type_hold = ttk.Radiobutton(self.noteFrame, text='Hold', variable=self.noteType, value='hold')
+        type_hold.grid(row=2, column=2, rowspan=1, columnspan=2)
+        type_avoid = ttk.Radiobutton(self.noteFrame, text='Avoid', variable=self.noteType, value='avoid')
+        type_avoid.grid(row=2, column=3, rowspan=1, columnspan=2)
 
+        # rail /label/radioButton
+        ttk.Label(self.noteFrame,text='轨道(Rail):').grid(row=4, column=1, rowspan=1, columnspan=2)
+        self.rail = IntVar()
+        for i in range(-2,3):
+            rbtn = ttk.Radiobutton(self.noteFrame, text=str(i), variable=self.rail, value=i)
+            rbtn.grid(row=4, column=5+i, rowspan=1, columnspan=1)
+
+        #NoteInfo /label/Entry
+        self.startBeat = DoubleVar()
+        self.touchBeat = DoubleVar()
+        self.timeLengthBeat = DoubleVar()
+        ttk.Label(self.noteFrame, text="生成拍子(SpawnBeat):").grid(row=5, column=1, rowspan=1, columnspan=2)
+        ttk.Entry(self.noteFrame, textvariable=self.startBeat).grid(row=5, column=3, rowspan=1, columnspan=4)
+        ttk.Label(self.noteFrame, text="触线拍子(TouchBeat):").grid(row=6, column=1, rowspan=1, columnspan=2)
+        ttk.Entry(self.noteFrame, textvariable=self.touchBeat).grid(row=6, column=3, rowspan=1, columnspan=4)
+        ttk.Label(self.noteFrame, text="时间长度(TimeLengthBeat):").grid(row=7, column=1, rowspan=1, columnspan=2)
+        ttk.Entry(self.noteFrame, textvariable=self.timeLengthBeat).grid(row=7, column=3, rowspan=1, columnspan=4)
+        #button add
+        add_btn = ttk.Button(self.noteFrame,text="ADD!",command=self.add_note_to_data)
+        add_btn.grid(row=8, column=3,  rowspan=2, columnspan=4)
+        self.root.bind("<Return>", self.add_note_to_data)
+        self.root.mainloop()
+
+    def add_note_to_data(self,*args):
+        self.note_info_check()
+        print("add note:")
+        print("Type: "+self.noteType.get())
+        print("Rail: "+str(self.rail.get()))
+        print("start beat: " + str(self.startBeat.get()))
+        print("touch Beat: " + str(self.touchBeat.get()))
+        print("time length Beat: " + str(self.timeLengthBeat.get()))
+
+    def note_info_check(self):
+        if(self.noteType.get()=="flick"):
+            self.timeLengthBeat.set(0)
+
+
+if __name__ == "__main__":
+    CLS_NoteEditor()
+"""
 feet = StringVar()
 feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
 feet_entry.grid(column=2, row=1, sticky=(W, E))
@@ -43,3 +82,4 @@ feet_entry.focus()
 root.bind("<Return>", calculate)
 
 root.mainloop()
+"""
--- tk-example.py
+++ tk-example.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 
-root = Tk()
+"""root = Tk()
 
 content = ttk.Frame(root)
 frame = ttk.Frame(content, borderwidth=5, relief="ridge", width=200, height=100)
@@ -28,4 +28,45 @@ three.grid(column=2, row=3)
 ok.grid(column=3, row=3)
 cancel.grid(column=4, row=3)
 
+root.mainloop()"""
+#---
+class FeetToMeters:
+
+    def __init__(self, root):
+
+        root.title("Feet to Meters")
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+        mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+
+        self.feet = StringVar()
+        feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
+        feet_entry.grid(column=2, row=1, sticky=(W, E))
+        self.meters = StringVar()
+
+        ttk.Label(mainframe, textvariable=self.meters).grid(column=2, row=2, sticky=(W, E))
+        ttk.Button(mainframe, text="Calculate", command=self.calculate).grid(column=3, row=3, sticky=W)
+
+        ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+        ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+        ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+        for child in mainframe.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+
+        feet_entry.focus()
+        root.bind("<Return>", self.calculate)
+
+    def calculate(self, *args):
+        try:
+            value = float(self.feet.get())
+            self.meters.set(int(0.3048 * value * 10000.0 + 0.5) / 10000.0)
+        except ValueError:
+            pass
+
+
+root = Tk()
+FeetToMeters(root)
 root.mainloop()
