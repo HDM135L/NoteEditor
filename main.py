d3da923cb95c507f19294e8adea238afb8df5a56
Tom <tomwxh@163.com>
2022-05-07 02:07:01 +0800

    add: main.py as entry point of program

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from Managers import CLS_DataManager
+from TKinterface import CLS_NoteEditor
 
+if __name__ == "__main__":
+    DM = CLS_DataManager("./Charts/StillAlive")
+    CM = DM.chartManagers["Easy"]
+    CLS_NoteEditor(CM)
