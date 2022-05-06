b8e7370d3e3062bb6ec10dc618f13675587d68a0
Tom <tomwxh@163.com>
2022-05-06 14:11:25 +0800

    Add(json):chart manager class and data manager class modify:saver and reader

---
+++ Managers.py
@@ -0,0 +1,60 @@
+from jsonIO import CLS_JsonReader,CLS_JsonSaver
+import os
+
+class CLS_ChartManager(CLS_JsonSaver):
+    def __init__(self,path):
+        super(CLS_ChartManager, self).__init__(path)
+        reader = CLS_JsonReader(path)
+
+        self.chartData = reader.get_content()
+        self.chartPath = path
+
+    def save_chart(self):
+        self.save_content(self.chartData)
+
+    def add_note(self,noteDict):
+        self.chartData["NoteNum"]+=1
+        self.chartData["NoteList"].append(noteDict)
+
+class CLS_DataManager(object):
+    def __init__(self, rootpath):
+        """
+        Manage all json data of a song.
+        :param rootpath: the root directory path, should be ./Charts/SongName
+        """
+        self.rootpath = rootpath
+        reader = CLS_JsonReader()
+        #get metadata
+        self.metapath = os.path.join(rootpath, "meta.json")
+        reader.reread(self.metapath)
+        self.metadata = reader.get_content()
+        self.meta_saver = CLS_JsonSaver(self.metapath)
+
+        #get charts
+        self.chartManagers = {} #difficulty name to CLS_ChartManager instance
+        for difficulty in self.metadata["Difficulties"]:
+            dname = difficulty["DifficultyName"]
+            chart_identifier = "chart"+difficulty.["Difficulty"]+".json"
+            chartpath = os.path.join(rootpath, chart_identifier)
+
+            self.chartManagers[dname] = CLS_ChartManager(chartpath)
+
+        reader.close()
+
+    def save_all(self):
+        '''
+        should not be used when lock feature is used.
+        :return:
+        '''
+        self.save_meta()
+        for CM in self.chartManagers:
+            CM.save_chart()
+        print(f"Saving complete.Saved meta and {len(self.chartManagers)} charts")
+        return
+
+    def save_meta(self):
+        self.meta_saver.save_content(self.metadata)
+
+    def save_chart_by_difficulty(self, difficulty):
+        self.chartManagers[difficulty].save_chart()
+
--- jsonIO.py
+++ jsonIO.py
@@ -1,9 +1,20 @@
 import json
 import os
 
+class StaticContentError(Exception):
+    pass
+class NoContentError(Exception):
+    pass
 
 class CLS_JsonReader(object):
-    def __init__(self, path: str):
+    def __init__(self, path=None):
+        '''
+        a reader can be reused for reading multiple files
+
+        :param path: optional, path to the file to read
+        '''
+        if path == None:
+            return
         self.f = open(path, 'r', encoding='utf-8')
         content = self.f.read()
         # print(content)
@@ -15,27 +26,49 @@ class CLS_JsonReader(object):
     def get_content(self):
         return self.jsDict
 
+    def reread(self, path: str):
+        self.f = open(path, 'r', encoding='utf-8')
+        content = self.f.read()
+        self.jsDict = json.loads(content)
+        print("Successfully reloaded json from path")
+        return
+
+    def close(self):
+        self.f.close()
     # def setContentByKey(self,key,value):
     #     self.jsDict[key]=value
     #     print(self.jsDict)
 
 
 class CLS_JsonSaver(object):
-    def __init__(self, path: str, content: dict = ""):
-        self.content = content
+    def __init__(self, path: str):
+        '''
+        a saver is bind to one json file
+
+        :param path: the path to the target json file
+        '''
         self.path = path
+        self.locked = False
+
+    def lock(self):
+        self.locked = True
+
+    def unlock(self):
+        self.locked = False
+
+    def save_and_lock(self,content):
+        self.save_content(content)
+        self.lock()
         return
 
-    def update_content(self, content):
-        self.content = content
-        return
-
-    def save_content(self,content=None):
-        if content:
-            self.content = content
-        with open(self.path, 'w', encoding='utf-8') as f:
-            f.write(json.dumps(self.content, indent=4))
-            print("Successfully saved!")
+    def save_content(self, content=None):
+        if self.locked:
+            print("file locked,saving fail")
+            return
+        else:
+            with open(self.path, 'w', encoding='utf-8') as f:
+                f.write(json.dumps(content, indent=4))
+                print("Successfully saved content parameter!")
         return
 
 
@@ -44,6 +77,6 @@ if __name__ == '__main__':
     metapath = "./Charts/StillAlive/meta.json"
     loader = CLS_JsonReader(metapath)
     content = loader.get_content()
-    content["Name"]="Still Alive"
-    saver = CLS_JsonSaver(metapath,content)
+    content["Name"] = "Still Alive"
+    saver = CLS_JsonSaver(metapath, content)
     saver.save_content()
